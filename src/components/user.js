39c4412f9038107776cacebecf0d3283e28bd10a
Trish Wynne <trishasone@gmail.com>
2020-09-17 22:03:44 +0100

    styling fixes and correctly import loading logo
    Co-authored-by: Glenroy-Terence (Terry) <glenroyterence@gmail.com>

--- src/components/user.js
+++ src/components/user.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../App.css";
 import getData from "../utils/dataapi.js";
+import loading from "../snake-game-logo.png";
 
 const User = (props) => {
   // const [userInput, setUserInput] = React.useState("");
@@ -11,9 +12,7 @@ const User = (props) => {
   });
 
   if (!props.userData) {
-    return (
-      <img alt="Original snake logo easter egg" src="../snake-game-logo.png" />
-    );
+    return <img alt="Original snake logo easter egg" src={loading} />;
   }
   console.log(props.userData);
   console.log(props.userData.avatar_url);
@@ -25,6 +24,7 @@ const User = (props) => {
         src={props.userData.avatar_url}
       />
       <h1 id="name">Hungry, {props.userData.login}?</h1>
+      <p>Use your arrow keys to get that tasty React</p>
     </section>
   );
 };
